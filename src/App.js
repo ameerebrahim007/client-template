31344e9d74fd1b9e1a63d38593c9fcaef0849d66
ameerebrahim007 <divya.raj@panaroma.finance>
2023-08-24 21:39:45 +0400

    Web to App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 
 function App() {
+
+  const [ data, setData ] = useState([''])
 
 useEffect(() => {
   window.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'dataFromRN') {
       const receivedData = event.data.payload;
+      setData(JSON.parse(receivedData));
       console.log('Received data from React Native:', receivedData);
   }
   });
 })
 
+const handleClick= () => {
+  // const sendDataToRN = () => {
+    const dataToSend = {
+      key1: 'Web to App',
+      key2: 'value2',
+    };
+    
+    window.postMessage(JSON.stringify(dataToSend), '*');
+  // };
+}
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button id="message">Click hete</button>
+        {
+          data.subTitle
+        }
+        <button id="message" onClick={handleClick}>Button1</button>
+        {/* <button id="message">Button2</button> */}
       </header>
     </div>
   );
