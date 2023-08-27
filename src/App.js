04392e51a9bbc817e8d77776f787c743c9ffdcd4
ameerebrahim007 <divya.raj@panaroma.finance>
2023-08-27 18:55:01 +0400

    listner n print

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
-
 import './App.css';
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
 
-  const [ data, setData ] = useState([''])
+  useEffect(() => {
+    // Define the receiveDataFromReactNative function
+    window.receiveDataFromReactNative = (dataString) => {
+      const data = JSON.parse(dataString);
+      console.log('Received data from React Native:', data);
+      // Process the received data in your web app
+      setData(data);
+    };
 
-// useEffect(() => {
-  window.addEventListener('message', (event) => {
-    console.log("data hereeeeee");
-    if (event.data && event.data.type === 'dataFromRN') {
-      const receivedData = event.data.payload;
-      // const receivedData = {
-      //   title: 'Web to App',
-      //   key2: 'value2',
-      // };
+    // Add an event listener to listen for messages from React Native
+    window.addEventListener('message', (event) => {
+      console.log('data hereeeeee');
+      if (event.data && event.data.type === 'dataFromRN') {
+        const receivedData = event.data.payload;
+        setData(JSON.parse(receivedData));
+        console.log('Received data from React Native:', receivedData);
+      }
+    });
+  }, []);
 
-      setData(JSON.parse(receivedData));
-      // setData(receivedData);
-
-      console.log('Received data from React Native:', receivedData);
-  }
-  });
-// },[])
-
-const handleClick= () => {
-  // const sendDataToRN = () => {
+  const handleClick = () => {
+    // Prepare data to send to React Native
     const dataToSend = {
       key1: 'Web to App',
       key2: 'value2',
     };
-    
-    window.postMessage(JSON.stringify(dataToSend), '*');
-  // };
-}
 
+    // Send the data to React Native
+    window.postMessage(JSON.stringify(dataToSend), '*');
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-      <span>{data}</span>
-        {
-          data.title
-        }
-        <button id="message" onClick={handleClick}>Button1</button>
-        <div id="container"></div>
-        {/* <button id="message">Button2</button> */}
+        <span>{data.key1}</span>
+        <button id="message" onClick={handleClick}>
+          Button1
+        </button>
+        <div id="container">hi</div>
       </header>
     </div>
   );
